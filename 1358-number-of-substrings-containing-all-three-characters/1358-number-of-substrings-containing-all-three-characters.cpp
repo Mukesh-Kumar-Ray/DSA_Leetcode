18b534109878a55c3aafe844db1e6fd4cfafd454
Mukesh Kumar Ray <123766305+Mukesh-Kumar-Ray@users.noreply.github.com>
2025-10-11 16:28:48 +0530

    Time: 47 ms (5.28%), Space: 16.9 MB (8.24%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -1,51 +1,36 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        // int i=0;
-        // int j=0;
-        // int cnt=0;
-        // int n=s.size();
+        int i=0;
+        int j=0;
+        int cnt=0;
+        int n=s.size();
 
-        // unordered_map<char,int>ump;
+        unordered_map<char,int>ump;
 
-        // while(j<n){
-        //     ump[s[j]]++;
+        while(j<n){
+            ump[s[j]]++;
 
-        //     if(ump.size()<3){
-        //         j++;
-        //     }
-        //     else if(ump.size() == 3){
-        //         cnt=cnt+1+(n-j-1);
-        //         while(ump.size() == 3){
-        //             ump[s[i]]--;
-        //             if(ump[s[i]]==0){
-        //                 ump.erase(s[i]);
-        //             }
-        //             if(ump.size()==3){
-        //                 cnt=cnt+1+(n-j-1);
-        //             }
-        //             i++;
-        //         }
-        //      j++;
-        //     }
-
-        // } 
-        // return cnt;       
-
-
-
-        unordered_map<char,int> map;
-        int count = 0, start = 0, end = 0;
-        int n = s.size();
-        while(end<n){
-            map[s[end]]++;
-            while( map['a'] && map['b'] && map['c'] ){
-                count+=n-end;
-                map[s[start]]--;
-                start++;
+            if(ump.size()<3){
+                j++;
             }
-            end++;
-        }
-        return count;
+            else if(ump.size() == 3){
+                cnt=cnt+1+(n-j-1);
+                while(ump.size() == 3){
+                    ump[s[i]]--;
+                    if(ump[s[i]]==0){
+                        ump.erase(s[i]);
+                    }
+                    if(ump.size()==3){
+                        cnt=cnt+1+(n-j-1);
+                    }
+                    i++;
+                }
+             j++;
+            }
+
+        } 
+        return cnt;       
+
     }
 };
