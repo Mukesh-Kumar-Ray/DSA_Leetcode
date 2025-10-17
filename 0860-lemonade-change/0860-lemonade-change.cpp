2f3c93972c520c093d06aa7ad6463eb29ab87599
Mukesh Kumar Ray <123766305+Mukesh-Kumar-Ray@users.noreply.github.com>
2025-10-17 15:35:33 +0530

    Time: 0 ms (100%), Space: 87.2 MB (71.35%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.cpp
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+// recursive solution 
     bool solveRE(vector<int>& bills, int i, int five, int ten) {
         if (i == bills.size()) {
             return true;
@@ -22,6 +23,30 @@ public:
         return false;
     }
     bool lemonadeChange(vector<int>& bills) {
-        return solveRE(bills, 0, 0, 0);
+        //return solveRE(bills, 0, 0, 0);
+
+        //greedy approach 
+        int fives = 0, tens = 0;
+        for (int bill : bills) {
+            if (bill == 5) {
+                fives++;
+            } else if (bill == 10) {
+                if (fives == 0) {
+                    return false;
+                }
+                fives--;
+                tens++;
+            } else {
+                if (tens > 0 && fives > 0) {
+                    tens--;
+                    fives--;
+                } else if (fives >= 3) {
+                    fives -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 };
