5fc714ee239169a33752ffc95a4270764a166ba4
Mukesh Kumar Ray <123766305+Mukesh-Kumar-Ray@users.noreply.github.com>
2026-09-03 17:47:12 +0530

    Time: 570 ms (96.59%), Space: 0B (100%) - LeetHub

---
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -0,0 +1,18 @@
+# Write your MySQL query statement below
+
+
+
+
+DELETE FROM Person
+WHERE id IN (
+    SELECT id
+    FROM (
+        SELECT id,
+               ROW_NUMBER() OVER (
+                   PARTITION BY email
+                   ORDER BY id
+               ) AS rn
+        FROM Person
+    ) AS temp
+    WHERE rn > 1
+);
