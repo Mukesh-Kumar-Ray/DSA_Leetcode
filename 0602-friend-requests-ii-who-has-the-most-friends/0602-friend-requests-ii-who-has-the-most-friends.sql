6c41a953b8cae1504145f865010ecd91d4d64baa
Mukesh Kumar Ray <123766305+Mukesh-Kumar-Ray@users.noreply.github.com>
2026-09-09 12:12:40 +0530

    Time: 292 ms (95.02%), Space: 0B (100%) - LeetHub

--- 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -1,35 +1,35 @@
 # Write your MySQL query statement below
--- WITH mrg_data AS(select requester_id ,accepter_id 
--- from RequestAccepted 
+WITH mrg_data AS(select requester_id ,accepter_id 
+from RequestAccepted 
 
--- union all
+union all
 
--- select accepter_id ,requester_id 
--- from RequestAccepted )
+select accepter_id ,requester_id 
+from RequestAccepted )
 
--- select mrgdata.requester_id as id ,count(mrgdata.requester_id) as num
--- from mrg_data mrgdata
--- group by mrgdata.requester_id 
--- order by num desc 
--- limit 1
+select mrgdata.requester_id as id ,count(mrgdata.requester_id) as num
+from mrg_data mrgdata
+group by mrgdata.requester_id 
+order by num desc 
+limit 1
 
 
-with cte as (
-    (
-        select requester_id as id, count(requester_id) as partial_count
-        from RequestAccepted
-        group by requester_id
-    )
-    union all
-    (
-        select accepter_id as id, count(accepter_id) as partial_count
-        from RequestAccepted
-        group by accepter_id
-    )
-)
+-- with cte as (
+--     (
+--         select requester_id as id, count(requester_id) as partial_count
+--         from RequestAccepted
+--         group by requester_id
+--     )
+--     union all
+--     (
+--         select accepter_id as id, count(accepter_id) as partial_count
+--         from RequestAccepted
+--         group by accepter_id
+--     )
+-- )
 
-select id, sum(partial_count) as num
-from cte
-group by id
-order by num desc
-limit 1;
+-- select id, sum(partial_count) as num
+-- from cte
+-- group by id
+-- order by num desc
+-- limit 1;
